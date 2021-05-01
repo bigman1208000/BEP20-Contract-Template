8011cac77cc0bbfae932e220dd4cd33353ecd095
B.M <bigman1208000@gmail.com>
2021-05-01 20:03:52 +0530

    Create BEP20_With_Tax.sol

---
+++ BEP20_With_Tax.sol
@@ -0,0 +1,10 @@
+Feature contract:
+
+- Tax fees for all transaction
+- Auto burn
+- Reward for holder
+- etc
+
+Each transaction will be subject to 2% tax and tax revenue will be divided into 2, namely: 1% for automatic burning and 1% to be distributed to all holders.
+
+The stop for auto burn can be determined, for example the auto burn will stop if the total supply remains 1 million from the total supply of 10 million.
